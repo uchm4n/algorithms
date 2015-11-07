3d8976bd0c6bc07a12eb73ccc707dd5efe4de80c
ucha19871 <ucha19871@gmail.com>
2015-11-07 19:41:55 +0000

    template

--- resources/views/nQueen/index.blade.php
+++ resources/views/nQueen/index.blade.php
@@ -1,9 +1,30 @@
 
 <html>
 	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 		<link rel="stylesheet" type="text/css" href="../css/app.css">
 	</head>
-	<body>
-		{!!$out!!}
+	<body class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="jumbotron">
+					<h2>Inser parameter in URL</h2>
+				</div>
+			</div>
+		</div>
+		
+		
+		<div class="row">
+			<div class="col-md-12 chess">
+				{!!$out!!}
+			</div>
+		</div>
+		
+		
+		<div class="row">
+			<br>
+		</div>
 	</body>
 </html>
